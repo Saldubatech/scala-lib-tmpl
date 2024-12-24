bfdfb355f58206b51fd677c66eae38c89727d1ee
Miguel Pinilla <jmpicnic@gmail.com>
2024-12-24 01:54:31 -0800

    Moved the Health Check to the common components and migrated it to Endpoint based definition.

--- components/common/src/main/scala/com/saldubatech/healthcheck/HealthCheckRoutes.scala
+++ components/common/src/main/scala/com/saldubatech/healthcheck/HealthCheckRoutes.scala
@@ -2,10 +2,33 @@ package com.saldubatech.healthcheck
 
 import zio.*
 import zio.http.*
+import zio.http.codec.*
+import zio.http.codec.PathCodec.path
+import zio.http.endpoint.Endpoint
+import zio.http.endpoint.openapi.{OpenAPI, OpenAPIGen, SwaggerUI}
 
 class HealthCheckRoutes(svc: HealthCheckService.Interface):
 
-  val routes: Routes[HealthCheckService.Interface, Nothing] = Routes(
+  private given noContentStatusCodec: StatusCodec[Unit] = HttpCodec.status(Status.NoContent)
+
+  val headEndpoint = Endpoint(Method.HEAD / "healthcheck").out[Unit](Status.NoContent)
+  val getEndpoint  = Endpoint(Method.GET / "healthcheck").out[String].outError[String](Status.InternalServerError)
+
+  def openAPI: OpenAPI = OpenAPIGen.fromEndpoints(title = "Healthcheck", version = "1.0.0", headEndpoint, getEndpoint)
+  val swaggerRoutes    = SwaggerUI.routes("docs" / "openapi" / "1.0.0" / "healthcheck", openAPI)
+
+  private val functionalRoutes = headEndpoint.implementAs(ZIO.unit).toRoutes ++ (getEndpoint
+    .implementHandler(
+      handler(svc.check.flatMap { dbStatus =>
+        if dbStatus.status then ZIO.succeed("A_OK")
+        else ZIO.fail("NOK")
+      })
+    )
+    .toRoutes)
+
+  val routes = functionalRoutes ++ swaggerRoutes
+
+  val routes0: Routes[HealthCheckService.Interface, Nothing] = Routes(
     Method.HEAD / "healthcheck" -> handler { (req: Request) =>
       ZIO.succeed(Response.status(Status.NoContent))
     },
--- lib/src/main/scala/com/saldubatech/infrastructure/network/oas3/entity/Oas3Component.scala
+++ lib/src/main/scala/com/saldubatech/infrastructure/network/oas3/entity/Oas3Component.scala
@@ -4,14 +4,14 @@ import com.saldubatech.infrastructure.network.Network
 import com.saldubatech.infrastructure.network.Network.ServiceLocator
 import com.saldubatech.infrastructure.services.{Entity, EntityService}
 import com.saldubatech.lang.types.meta.MetaType
-import zio.{URLayer, ZIO, ZLayer, Tag as ZTag}
+import zio.{Tag as ZTag, URLayer, ZIO, ZLayer}
 
 trait Oas3Component[E <: Entity: ZTag: MetaType, SUMMARY: ZTag: MetaType](
     using summarizer: Option[Adaptor.Summarizer[E, SUMMARY]] = None):
 
   type ES <: EntityService[E]
 
-  class Endpoint(override val name: String, override val version: String)              extends EntityEndpoint[E, SUMMARY]
+  class Endpoint(override val name: String, override val version: String)                    extends EntityEndpoint[E, SUMMARY]
   class ServiceAdaptor(override val service: ES, override val forEndpoint: Network.Endpoint) extends Adaptor[E, SUMMARY]
 
   final type Routing = Routes[E, SUMMARY, ServiceAdaptor]
